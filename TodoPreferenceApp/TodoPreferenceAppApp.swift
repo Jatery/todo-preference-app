dd56408d95a8d00c4ed131359208235d0b4637d2
Jatery <cyz3827@gmail.com>
2025-05-08 00:26:25 +0800

    Initial Commit

---
+++ TodoPreferenceApp/TodoPreferenceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoPreferenceAppApp.swift
+//  TodoPreferenceApp
+//
+//  Created by 陳彥璋 on 2025/5/8.
+//
+
+import SwiftUI
+
+@main
+struct TodoPreferenceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
